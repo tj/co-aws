f73eb32d4a3060575fa92f6df2315e6e6a9c9b05
Nick Poorman <nickpoorman@gmail.com>
2014-06-19 12:26:21 -0400

    just use the wrap function

--- index.js
+++ index.js
@@ -4,42 +4,21 @@
  */
 
 var thunkify = require('thunkify');
-var assert = require('assert');
-var aws = require('aws-sdk');
 
 /**
  * Expose `Client`.
  */
 
-module.exports = Client;
-
-/**
- * AWS client.
- *
- * @param {Object} [opts]
- * @api public
- */
-
-function Client(opts) {
-  if (!(this instanceof Client)) return new Client(opts);
-
-  // global?
-  aws.config.update(opts);
-
-  // wayyyy more to support...
-  this.ec2 = new aws.EC2;
-  wrap(this.ec2);
-}
-
 /**
  * Wrap `obj` methods.
  */
 
-function wrap(obj) {
+module.exports = function wrap(obj) {
   Object.keys(obj.__proto__).forEach(function(key){
     var val = obj.__proto__[key];
     if ('constructor' == val) return;
     if ('function' != typeof val) return;
     obj[key] = thunkify(obj[key]);
   });
+  return obj;
 }
